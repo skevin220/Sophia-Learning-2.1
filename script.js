483fbf451ff3c14235e5ed3b2fc9a559fdf59d84
Kevin Stacey <97564746+skevin220@users.noreply.github.com>
2026-02-12 13:24:08 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+
+function subscribeAlert() {
+  alert("Thank you for subscribing.");
+}
+
+function addToCartAlert(itemName) {
+  let cart = JSON.parse(sessionStorage.getItem("cart")) || [];
+  cart.push(itemName);
+  sessionStorage.setItem("cart", JSON.stringify(cart));
+
+  alert("Item added to the cart.");
+}
+
+function viewCart() {
+  let cart = JSON.parse(sessionStorage.getItem("cart")) || [];
+
+  if (cart.length === 0) {
+    alert("Your cart is empty.");
+    return;
+  }
+
+  alert("Items in your cart:\n\n" + cart.join("\n"));
+}
+
+function clearCartAlert() {
+  sessionStorage.removeItem("cart");
+  alert("Cart cleared.");
+}
+
+function processOrderAlert() {
+  sessionStorage.removeItem("cart");
+  alert("Thank you for your order.");
+}
+
+
+function messageAlert(event) {
+  event.preventDefault();
+
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const message = document.getElementById("request").value;
+
+  localStorage.setItem("customerName", name);
+  localStorage.setItem("customerEmail", email);
+  localStorage.setItem("customerMessage", message);
+
+  alert("Thank you for your message.");
+}
